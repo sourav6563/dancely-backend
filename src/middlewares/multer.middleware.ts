d4848164fb2f598db2e88c9381e4f6f099ea0178
sourav <souravmahato6563@gmail.com>
2026-02-05 15:49:51 +0530

    feat: Create Multer upload directory if it doesn't exist and add .gitkeep.

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,8 +1,14 @@
 import multer from "multer";
 
+import fs from "fs";
+
 const storage = multer.diskStorage({
   destination: function (_req, _file, cb) {
-    cb(null, "./public/temp");
+    const uploadPath = "./public/temp";
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+    cb(null, uploadPath);
   },
   filename: function (_req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
