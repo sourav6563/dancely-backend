cb616a4dc254eb5a7902b91f879bc593127cc464
sourav <souravmahato6563@gmail.com>
2026-02-07 23:31:54 +0530

    .

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import { env } from "./env";
 export const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: env.NODE_ENV === "production",
-  sameSite:"lax",
+  sameSite:"none",
   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   path: "/",
 } as const;
